e57d0e7e6e143901e42073bb158345190c080f07
Darius <dariuskumza@gmail.com>
2023-11-08 15:41:06 +0200

    splitting code to more functions

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const printToDOM = (drink) => {
+// * function to create single cocktail item to DOM
+const printToDOM = (drink, ingredients) => {
    document.querySelector(".result").innerHTML = `
             <div class="col-6">
                 <img src="${drink.strDrinkThumb}" alt="${drink.strDrink}" />
@@ -7,13 +8,13 @@ const printToDOM = (drink) => {
                 <h2>${drink.strDrink}</h2>
                 <p>${drink.strInstructions}</p>
                 <ul>
-                    <li> Type: <button class="btn btn-outline-primary btn-sm"> ${
+                    <li> Type: <button class="btn btn-outline-success btn-sm"> ${
                        drink.strAlcoholic
                     }</button></li>
-                    <li>Category: <button class="btn btn-outline-primary btn-sm"> ${
+                    <li>Category: <button class="btn btn-outline-success btn-sm"> ${
                        drink.strCategory
                     } </button></li>
-                    <li>Glass Type: <button class="btn btn-outline-primary btn-sm"> ${
+                    <li>Glass Type: <button class="btn btn-outline-success btn-sm"> ${
                        drink.strGlass
                     }</button></li>
                     <li class="iba">IBA: ${drink.strIBA}</li>
@@ -38,6 +39,19 @@ const printToDOM = (drink) => {
       : imgAtr.classList.remove("hide");
 };
 
+// * function to work with ingredients from API
+const drinkIngredients = (drink, ingredients) => {
+   for (let i = 1; i <= 15; i++) {
+      if (drink["strIngredient" + i]) {
+         const measure = drink["strMeasure" + i] ? drink["strMeasure" + i] : "";
+         ingredients[ingredients.length] = `
+              <li>${drink["strIngredient" + i]} ${measure}</li>
+          `;
+      }
+   }
+};
+
+// * fetches data from API to display as search results to DOM
 function searchCocktails(e) {
    e.preventDefault();
 
@@ -51,14 +65,12 @@ function searchCocktails(e) {
          document.querySelector(".result").innerHTML = resp.drinks
             .map(
                (drink) => `
-            <div class="cocktail col-4 mb-3" onclick="showCocktail(${
-               drink.idDrink
-            })">
+            <div class="cocktail col-4  border rounded p-4 hover" onclick="showCocktail(${drink.idDrink})">
                 <img src="${drink.strDrinkThumb}" alt="${drink.strDrink}" />
-                <h3>${drink.strDrink}</h3>
-                <div class="d-flex justify-content-between">
-                    <span>${drink.dateModified ? drink.dateModified : ""}</span>
-                    <span>${drink.strAlcoholic}</span>
+               
+                <div class="d-flex justify-content-between align-content-center align-items-center mt-2">
+                    <h3 class="mb-0">${drink.strDrink}</h3>   
+                    <h6 class="mb-0">${drink.strAlcoholic}</h6>
                 </div>
             </div>
         `
@@ -67,6 +79,7 @@ function searchCocktails(e) {
       });
 }
 
+// * show cocktail by id, by clicking on single searched cocktail item
 function showCocktail(id) {
    fetch("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + id)
       .then((resp) => resp.json())
@@ -74,17 +87,9 @@ function showCocktail(id) {
          const drink = resp.drinks[0];
          const ingredients = [];
 
-         for (let i = 1; i <= 15; i++) {
-            if (drink["strIngredient" + i]) {
-               const measure = drink["strMeasure" + i]
-                  ? drink["strMeasure" + i]
-                  : "";
-               ingredients[ingredients.length] = `
-                    <li>${drink["strIngredient" + i]} ${measure}</li>
-                `;
-            }
-         }
-         printToDOM(drink);
+         drinkIngredients(drink, ingredients);
+
+         printToDOM(drink, ingredients);
       });
 }
 
@@ -96,63 +101,18 @@ function imLucky() {
       .then((resp) => resp.json())
       .then((resp) => {
          if (!resp.drinks) return;
-         console.log(resp.drinks);
 
          const drink = resp.drinks[0];
          const ingredients = [];
 
-         for (let i = 1; i <= 15; i++) {
-            if (drink["strIngredient" + i]) {
-               const measure = drink["strMeasure" + i]
-                  ? drink["strMeasure" + i]
-                  : "";
-               ingredients[ingredients.length] = `
-                    <li>${drink["strIngredient" + i]} ${measure}</li>
-                `;
-            }
-         }
+         drinkIngredients(drink, ingredients);
 
-         document.querySelector(".result").innerHTML = `
-            <div class="col-6">
-                <img src="${drink.strDrinkThumb}" alt="${drink.strDrink}" />
-            </div>
-            <div class="col-6">
-                <h2>${drink.strDrink}</h2>
-                <p>${drink.strInstructions}</p>
-                <ul>
-                    <li> Type: <button class="btn btn-outline-primary btn-sm"> ${
-                       drink.strAlcoholic
-                    }</button></li>
-                    <li>Category: <button class="btn btn-outline-primary btn-sm"> ${
-                       drink.strCategory
-                    } </button></li>
-                    <li>Glass Type: <button class="btn btn-outline-primary btn-sm"> ${
-                       drink.strGlass
-                    }</button></li>
-                    <li class="iba">IBA: ${drink.strIBA}</li>
-                    <li class="img-atr">Image Atribution: ${
-                       drink.strImageAttribution
-                    }</li>
-                </ul>
-                <h4>Ingredients:</h4>
-                <ul>
-                    ${ingredients.join("")}
-                </ul>
-            </div>
-        `;
-         const ibaItem = document.querySelector(".iba");
-         !drink.strIBA
-            ? ibaItem.classList.add("hide")
-            : ibaItem.classList.remove("hide");
-
-         const imgAtr = document.querySelector(".img-atr");
-         !drink.strImageAttribution
-            ? imgAtr.classList.add("hide")
-            : imgAtr.classList.remove("hide");
+         printToDOM(drink, ingredients);
       });
 }
 
+// * i'm lucky button eventlistener
 btnLucky.addEventListener("click", (e) => {
-   e.preventDefault;
+   e.preventDefault();
    imLucky();
 });
